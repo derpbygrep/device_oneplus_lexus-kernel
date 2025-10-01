521cc06fc1086035e5810fec1179de50cf8b0fbe
grep <grepfox@tutamail.com>
2025-10-01 10:27:45 +0000

    remove duplicate sched_param redefination

--- kernel-headers/linux/sched/types.h
+++ kernel-headers/linux/sched/types.h
@@ -4,9 +4,9 @@
 
 #include <linux/types.h>
 
-struct sched_param {
-	int sched_priority;
-};
+//struct sched_param {
+//	int sched_priority;
+//};
 
 #define SCHED_ATTR_SIZE_VER0	48	/* sizeof first published struct */
 #define SCHED_ATTR_SIZE_VER1	56	/* add: util_{min,max} */
